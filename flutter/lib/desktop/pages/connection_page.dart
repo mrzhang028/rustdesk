b65f6d35c4db4fd75b2a6bef347c450a22a6c4bf
mrzhang028 <61349499+mrzhang028@users.noreply.github.com>
2025-08-14 20:36:19 +0800

    Update connection_page.dart
    更改个人服务器域名

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -41,7 +41,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   double? get height => bind.isIncomingOnly() ? null : em * 3;
 
   void onUsePublicServerGuide() {
-    const url = "https://www.kuntech.com";
+    const url = "https://www.kuntech.xyz";
     canLaunchUrlString(url).then((can) {
       if (can) {
         launchUrlString(url);
